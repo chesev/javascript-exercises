b5f11c9d9467b1f171fa79420574dee054c3e598
Cherry Severino <cheseverino19@gmail.com>
2022-08-11 13:33:38 +0800

    Passed all 7 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
+    if (num === 0) return '';
+    if (num === 1) return str;
 
+
+    return str + repeatString(str, num - 1); 
 };
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', Math.floor(Math.random()* 20));
+repeatString('', 0);
 
 // Do not edit below this line
 module.exports = repeatString;
